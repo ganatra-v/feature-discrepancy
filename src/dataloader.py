7985d2c7cf898a67a234d99911a056306a3b7514
ganatra-v <vaib2641@gmail.com>
2025-02-04 03:21:38 +0530

    [Update dataloader.py] Load 'two-moons' dataset

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,2 +1,24 @@
-def load_data(args):
+import numpy as np
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+
+def load_two_moons(args):
+    x, y = make_moons(n_samples = 5000, random_state= 42)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
+    return x_train, y_train, x_test, y_test
+
+
+def load_noisy_two_moons(args):
     pass
+
+
+def load_sonar(args):
+    pass
+
+def load_data(args):
+    if args.dataset == "two-moons":
+        return load_two_moons(args)
+    elif args.dataset == "noisy-two-moons":
+        return load_noisy_two_moons(args)
+    elif args.dataset == "sonar":
+        return load_sonar(args)
